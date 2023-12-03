02370095ca7f838ca256d93244b15f31140fdda4
Yuta Nakamura <nakamurayuta0204@gmail.com>
2023-12-03 18:06:18 +0900

    feat: setting up apollo client

---
+++ src/github/client.ts
@@ -0,0 +1,24 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+
+/**
+ * Returns an Apollo Client instance configured with the provided Github token.
+ * @returns {ApolloClient} The configured Apollo Client instance.
+ * @throws {Error} If the Github token is not provided in the environment variables.
+ */
+export const getClient = () => {
+  const token = process.env.GITHUB_TOKEN
+
+  if (!token) {
+    throw new Error('Please add your Github token to .env.local')
+  }
+
+  const client = new ApolloClient({
+    uri: 'https://api.github.com/graphql',
+    cache: new InMemoryCache(),
+    headers: {
+      authorization: `Bearer ${token}`
+    }
+  })
+
+  return client
+}
